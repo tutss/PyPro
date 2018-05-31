27a4f9019af848d25bf515fcc56b36c5428e047a
tutss <artur_santos@usp.br>
2018-05-31 17:11:45 -0300

    finished implementing qr factorization

--- dec_fac/qrfac.py
+++ dec_fac/qrfac.py
@@ -6,9 +6,8 @@ import scipy.linalg
 def qr_scipy_example():
     A = np.array([[12, -51, 4], [6, 167, -68], [-4, 24, -41]])  # From the Wikipedia Article on QR Decomposition
     Q, R = scipy.linalg.qr(A)
-    print(A)
-    print(Q)
-    print(R)
+    print('scipy q: \n', Q)
+    print('scipy r: \n', R)
 
 
 # My implementation
@@ -21,23 +20,41 @@ def normalize_vector(u):
 
 
 def multiply_matrix(M, N):
-    tupleM_size = M.shape
-    tupleN_size = N.shape
-    if (tupleM_size[1] != tupleN_size[0]):
-        return 'Cant multiply'
+    m, n = M.shape
+    m1, n1 = N.shape
+    if (n != m1):
+        return "Can't multiply"
     else:
         return np.matmul(M, N)
     return 0
 
 
-def householder(A):
-    return 0
+def qr(A):
+    m, n = A.shape
+    Q = np.eye(m)
+    for i in range(n - (m == n)):
+        H = np.eye(m)
+        H[i:, i:] = householder(A[i:, i])
+        Q = np.dot(Q, H)
+        A = np.dot(H, A)
+    return Q, A
+
+
+def householder(a):
+    v = a / (a[0] + np.copysign(np.linalg.norm(a), a[0]))
+    v[0] = 1
+    h = np.eye(a.shape[0])
+    h -= (2 / np.dot(v[None, :], v[:, None])) * np.dot(v[:, None], v[None, :])
+    return h
 
 
 def main():
-    A = np.array([[12, 4, -6], [3, 5, 12], [10, 23, 19]])
-    B = np.array([[13, 13, 12], [3, 2, 1], [1, 2, 1]])
-    R = multiply_matrix(A, B)
-    print(R)
+    A = np.array([[12, -51, 4], [6, 167, -68], [-4, 24, -41]])
+    Q, R = qr(A)
+    print('q:\n', Q.round(6))
+    print('r:\n', R.round(6))
+    print('-----\n')
+    qr_scipy_example()
+
 
 main()
